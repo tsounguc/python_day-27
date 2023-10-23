f45f63d701bc5de4f7ccedeb0c7bb84a0fcfae36
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-23 17:20:00 -0400

    **kwargs: Many Keywords Arguments

--- playground.py
+++ playground.py
@@ -1,3 +1,5 @@
+
+# args is a tuple
 def add(*args):
     total = 0
     for n in args:
@@ -6,3 +8,31 @@ def add(*args):
 
 
 print(add(3, 5, 6))
+
+
+# kwargs is a dictionary
+def calculate(**kwargs):
+    print(kwargs)
+    # for key in kwargs:
+    #     print(key)
+    #     print(kwargs[key])
+    for (key, value) in kwargs.items():
+        print(f"{key}: {value}")
+    print(kwargs["add"])
+
+
+calculate(add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kw):
+        # self.make = kw["make"]
+        # self.model = kw["model"]
+        # lines below do the same thing but don't throw an error is one of the attributes weren't set
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.make)
+print(my_car.model)
